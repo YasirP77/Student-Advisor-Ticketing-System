bdad8a8fbaea6d21f244594a18a583f930937d3c
YasirP77 <yasirpatel5313@gmail.com>
2025-04-09 14:53:17 -0400

    Sats - IT Expo
    Progress till the IT Expo demo

---
+++ app/_layout.tsx
@@ -0,0 +1,40 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { InactivityProvider } from './context/InactivityContext';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({ SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'), });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <InactivityProvider timeoutMinutes={2} warningSeconds={10}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="Login" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </InactivityProvider>
+  );
+}
